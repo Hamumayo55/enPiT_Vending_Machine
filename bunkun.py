0a0df21c2bb41ba55c9f07a0122510e6677e5d1d
Takuya Shinjo <e175769@ie.u-ryukyu.ac.jp>
2019-09-04 17:02:52 +0900

    modify  sprint2

--- bunkun.py
+++ bunkun.py
@@ -1,7 +1,14 @@
 def insert():
     #お金が入っているか判定する関数
-    money = int(input("お金を入れてください: "))
-    if money >= 300:
+    global MEMORYMONEY
+    try:
+        money = int(input("お金を入れてください: "))
+    except ValueError:
+        money = 0
+
+    MEMORYMONEY += money
+
+    if MEMORYMONEY >= 300:
         return payment(True)
     else :
         return payment(False)
@@ -15,14 +22,31 @@ def payment(judge):
 
 def dispense(item):
     #商品を排出する関数
+    global MEMORYMONEY
     if item == "シャーペン":
-        item_count = counter(True)
-        print("商品：シャーペン")
-        print("売り上げは"+str(item_count)+"yenです")
+        check_purchase = input("シャーペンを購入しますか(Yes/No): ")
+        while True:
+            if check_purchase == "Yes":
+                MEMORYMONEY -= 300
+                item_count = counter(True)
+                print("商品：シャーペン")
+                print("お釣りは"+str(MEMORYMONEY)+"yenです")
+                break
+
+            elif check_purchase == "No":
+                print("お釣りは"+str(MEMORYMONEY)+"yenです")
+                break
+
+            else:
+                check_purchase = input("Yes/Noで入力してください(Yes/No)：")
+
+        MEMORYMONEY = 0
+        #print("売り上げは"+str(item_count)+"yenです")
     else:
+        diff_money = 300 - MEMORYMONEY
         item_count = counter(False)
-        print("お金が足りないよ")
-        print("売り上げは"+str(item_count)+"yenです")
+        print(str(diff_money)+"yenのお金が足りないよ")
+        #print("売り上げは"+str(item_count)+"yenです")
 
 def counter(cnt):
     #商品をカウントする関数
@@ -35,14 +59,14 @@ def counter(cnt):
         sales = CNTITEM * 300
         return sales
     
-
 def main():
+    global MEMORYMONEY
     while True:
+        print(str("-"*40))
+        print('現在の投入金額は次の通りです:'+str(MEMORYMONEY)+"yenです")
         insert()
-
 
 if __name__ == "__main__":
     CNTITEM = 0
+    MEMORYMONEY = 0
     main()
-
-
