1693721653bf95dbf97a61e3f3c56db3bc769669
Takuya Shinjo <e175769@ie.u-ryukyu.ac.jp>
2019-09-05 13:04:27 +0900

    add sprint3

--- bunkun.py
+++ bunkun.py
@@ -1,22 +1,35 @@
-def insert():
+
+
+def insert(dict):
     #お金が入っているか判定する関数
     global MEMORYMONEY
+    display=dict
+    print(display)
+
+    
+    
+    select_number = input("買いたい商品の商品番号を選択してください")
+
     try:
-        money = int(input("お金を入れてください: "))
+        money = int(input(str(display[select_number][1])+"yen入れてください: "))
     except ValueError:
         money = 0
 
     MEMORYMONEY += money
 
-    if MEMORYMONEY >= 300:
-        return payment(True)
-    else :
-        return payment(False)
+    # for key,value in display.items():
+    #     if value[1] <= MEMORYMONEY:
+    #         print(key,value)
 
-def payment(judge):
+    if MEMORYMONEY >= display[select_number][1]:
+        return payment(True, display[select_number][0])
+    else :
+        return payment(False, display[select_number][0])
+
+def payment(judge, item):
     #商品が買えるか判定する関数
     if judge == True:
-        return dispense("シャーペン")
+        return dispense(item)
     else :
         return dispense("ダミー")
 
@@ -24,13 +37,15 @@ def dispense(item):
     #商品を排出する関数
     global MEMORYMONEY
     if item == "シャーペン":
-        check_purchase = input("シャーペンを購入しますか(Yes/No): ")
+        check_purchase = input(item+"を購入しますか(Yes/No): ")
+        
         while True:
             if check_purchase == "Yes":
                 MEMORYMONEY -= 300
-                item_count = counter(True)
-                print("商品：シャーペン")
+                item_count = counter(True,item)
+                print("商品："+item)
                 print("お釣りは"+str(MEMORYMONEY)+"yenです")
+                print("シャーペンの売り上げは"+str(item_count)+"yenです")
                 break
 
             elif check_purchase == "No":
@@ -39,34 +54,69 @@ def dispense(item):
 
             else:
                 check_purchase = input("Yes/Noで入力してください(Yes/No)：")
-
         MEMORYMONEY = 0
-        #print("売り上げは"+str(item_count)+"yenです")
+        
+    elif item == "消しゴム":
+        check_purchase = input(item+"を購入しますか(Yes/No): ")
+        
+        while True:
+            if check_purchase == "Yes":
+                MEMORYMONEY -= 200
+                item_count = counter(True,item)
+                print("商品："+item)
+                print("お釣りは"+str(MEMORYMONEY)+"yenです")
+                print("消しゴムの売り上げは"+str(item_count)+"yenです")
+                break
+
+            elif check_purchase == "No":
+                print("お釣りは"+str(MEMORYMONEY)+"yenです")
+                break
+
+            else:
+                check_purchase = input("Yes/Noで入力してください(Yes/No)：")
+        MEMORYMONEY = 0
+
     else:
         diff_money = 300 - MEMORYMONEY
-        item_count = counter(False)
+        item_count = counter(False,item)
         print(str(diff_money)+"yenのお金が足りないよ")
         #print("売り上げは"+str(item_count)+"yenです")
 
-def counter(cnt):
+def counter(cnt,item):
     #商品をカウントする関数
-    global CNTITEM 
-    if cnt:
-        CNTITEM += 1
-        sales = CNTITEM * 300
-        return sales
+    global CNTITEM_sy
+    global CNTITEM_kesi
+    if item == "シャーペン":
+        if cnt:
+            CNTITEM_sy += 1
+            sales = CNTITEM_sy * 300
+            return sales
+        else:
+            sales = CNTITEM_sy * 300
+            return sales
+    elif item == "消しゴム":
+        if cnt:
+            CNTITEM_kesi += 1
+            sales = CNTITEM_kesi * 200
+            return sales
+        else:
+            sales = CNTITEM_kesi * 200
+            return sales
     else:
-        sales = CNTITEM * 300
-        return sales
+        return 
+
     
 def main():
+    dict_bunkun={"1":["シャーペン",300], "2":["消しゴム",200]}
+    print(dict_bunkun["1"][0])
     global MEMORYMONEY
     while True:
         print(str("-"*40))
         print('現在の投入金額は次の通りです:'+str(MEMORYMONEY)+"yenです")
-        insert()
+        insert(dict_bunkun)
 
 if __name__ == "__main__":
-    CNTITEM = 0
+    CNTITEM_sy = 0
+    CNTITEM_kesi = 0
     MEMORYMONEY = 0
     main()
